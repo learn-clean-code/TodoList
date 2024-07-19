45bfcc1316b14ea435bb4155c54da91f1021aeb0
JaeBeen95 <nin2348@gmail.com>
2024-07-19 21:45:32 +0900

    style: 전체 container width 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ToDo from "./components/to-do"
 function App() {
   return (
     <div className="w-full h-screen py-32 bg-[#e7e6df]">
-      <div className="w-96 mx-auto bg-white rounded shadow-md">
+      <div className="relative w-1/2 h-3/4 mx-auto bg-white rounded shadow-md">
         <ToDo />
       </div>
     </div>
--- src/components/to-do/ToDoEditor.jsx
+++ src/components/to-do/ToDoEditor.jsx
@@ -1,10 +1,10 @@
-import { useInput } from '../../hooks/useInput'
+import { useInput } from "../../hooks/useInput"
 
 function ToDoEditor({ onCreate }) {
-  const { value, onChange, onKeyDown, handleSubmit } = useInput('', onCreate)
+  const { value, onChange, onKeyDown, handleSubmit } = useInput("", onCreate)
 
   return (
-    <div className="flex gap-10">
+    <div className="absolute bottom-0 left-0 right-0 flex gap-10 p-5">
       <input
         className="flex-1 p-2 border-2 border-gray-200 outline-green-300 rounded-lg"
         value={value}
