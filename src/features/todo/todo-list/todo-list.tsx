c05dd523acdce3486455831d9301ac5b941f64d5
d5ng <dong5ffice@gmail.com>
2024-07-21 12:53:11 +0900

    ✨ Feat: Todo List 추가

---
+++ src/features/todo/todo-list/todo-list.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { TodoListItem } from "features/todo"
+import type { Todo } from "features/todo"
+import classes from "./todo-list.module.css"
+
+interface Props {
+  todos: Todo[]
+  onUpdateTodo: (todoId: number) => void
+}
+
+export default function TodoList(props: Props) {
+  return (
+    <ul className={classes["todo-list"]}>
+      {props.todos.map((todo) => (
+        <TodoListItem key={todo.id} {...todo} onUpdateTodo={props.onUpdateTodo} />
+      ))}
+    </ul>
+  )
+}
