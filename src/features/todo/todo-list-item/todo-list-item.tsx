4ebb5b76a75b60c1d553c71d53233f721874115f
d5ng <dong5ffice@gmail.com>
2024-07-21 12:53:32 +0900

    ✨ Feat: Todo List Item 추가

---
+++ src/features/todo/todo-list-item/todo-list-item.tsx
@@ -0,0 +1,31 @@
+import { ChangeEventHandler } from "react"
+import { Input } from "shared/@common/components"
+import { useCheckbox } from "shared/@common/hooks"
+import type { Todo } from "features/todo"
+import classes from "./todo-list-item.module.css"
+
+export default function TodoListItem(props: Todo & { onUpdateTodo: (todoId: number) => void }) {
+  const { isChecked, onChangeChecked } = useCheckbox({ defaultValue: false })
+
+  const handleChangeChecked: ChangeEventHandler<HTMLInputElement> = (event) => {
+    onChangeChecked(event)
+    props.onUpdateTodo(props.id)
+  }
+
+  const listClassName = `${classes["todo-list-item"]} ${props.isDone ? classes["todo-list-item__active"] : ""}`
+
+  return (
+    <li className={listClassName}>
+      <label htmlFor={`todo-list-item-${props.id}`}>
+        <p className={classes["todo-list-item__contents"]}>{props.title}</p>
+      </label>
+      <Input
+        type="checkbox"
+        id={`todo-list-item-${props.id}`}
+        checked={isChecked}
+        onChange={handleChangeChecked}
+        className={classes["todo-list-item__checkbox"]}
+      />
+    </li>
+  )
+}
