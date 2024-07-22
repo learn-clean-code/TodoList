a37307638462402f6b4901282627b5c0d27a3d10
d5ng <dong5ffice@gmail.com>
2024-07-22 20:05:54 +0900

    🔥 Remove: 불 필요한 코드 삭제

--- src/shared/@common/hooks/use-input.ts
+++ src/shared/@common/hooks/use-input.ts
@@ -7,23 +7,17 @@ interface Props {
 
 export default function useInput(props: Props) {
   const [value, setValue] = useState(props.defaultValue || "")
-  const [isTouched, setIsTouched] = useState(false)
 
   const onChangeValue: ChangeEventHandler<HTMLInputElement> = (event) => setValue(event.target.value)
   const onResetValue = () => setValue("")
 
-  const onBlurTouched = () => setIsTouched(true)
-  const onResetTouched = () => setIsTouched(false)
-
   const isValid = props.validateFn ? props.validateFn(value) : true
-  const hasError = !isValid && isTouched
 
   return {
     value,
     onChangeValue,
     onResetValue,
-    onBlurTouched,
-    onResetTouched,
-    hasError,
+    isValid,
+    hasError: !isValid,
   }
 }
