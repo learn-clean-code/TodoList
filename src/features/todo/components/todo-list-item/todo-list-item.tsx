9f04bdf54eddfcbf3bef2c9cadb3eb18e4604d24
d5ng <dong5ffice@gmail.com>
2024-07-22 19:56:46 +0900

    🐛 Fix: TodoListItem 클릭이 제대로 안되는 문제 해결

--- src/features/todo/components/todo-list-item/todo-list-item.tsx
+++ src/features/todo/components/todo-list-item/todo-list-item.tsx
@@ -2,6 +2,7 @@ import { ChangeEventHandler } from "react"
 import { Input } from "shared/@common/components"
 import { useCheckbox } from "shared/@common/hooks"
 import type { Todo } from "features/todo/types"
+import { MdCheck } from "react-icons/md"
 import classes from "./todo-list-item.module.css"
 
 export default function TodoListItem(props: Todo & { onUpdateTodo: (todoId: number) => void }) {
@@ -12,20 +13,26 @@ export default function TodoListItem(props: Todo & { onUpdateTodo: (todoId: numb
     props.onUpdateTodo(props.id)
   }
 
-  const listClassName = `${classes["todo-list-item"]} ${props.isDone ? classes["todo-list-item__active"] : ""}`
+  const listClassName = `${classes["todo-list-item"]} ${props.isDone ? classes["todo-list-item__done"] : ""}`
 
   return (
     <li className={listClassName}>
-      <label htmlFor={`todo-list-item-${props.id}`}>
+      <label htmlFor={`todo-list-item-${props.id}`} className={classes["todo-list-item__label"]}>
         <p className={classes["todo-list-item__contents"]}>{props.title}</p>
       </label>
-      <Input
-        type="checkbox"
-        id={`todo-list-item-${props.id}`}
-        checked={props.isDone}
-        onChange={handleChangeChecked}
-        className={classes["todo-list-item__checkbox"]}
-      />
+      <div className={classes["todo-list-item__checkbox"]}>
+        {props.isDone && (
+          <label htmlFor={`todo-list-item-${props.id}`}>
+            <MdCheck color="#fff" />
+          </label>
+        )}
+        <Input
+          type="checkbox"
+          id={`todo-list-item-${props.id}`}
+          checked={props.isDone}
+          onChange={handleChangeChecked}
+        />
+      </div>
     </li>
   )
 }
