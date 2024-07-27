e738c858a10587d65c25d64a0101b0e4be3fc925
d5ng <dong5ffice@gmail.com>
2024-07-28 00:46:55 +0900

    ✨ Feat: Todo List Item 모션 적용

--- src/features/todo/components/todo-list-item/todo-list-item.tsx
+++ src/features/todo/components/todo-list-item/todo-list-item.tsx
@@ -1,9 +1,9 @@
 import { ChangeEventHandler } from "react"
-import { Input } from "shared/@common/components"
 import { useCheckbox } from "shared/@common/hooks"
 import type { Todo } from "features/todo/types"
-import { MdCheck } from "react-icons/md"
 import classes from "./todo-list-item.module.css"
+import { motion } from "framer-motion"
+import { checkboxAnimation, checkboxSVGAnimation, todoContentsAnimatmion } from "shared/@common/animation"
 
 export default function TodoListItem(props: Todo & { onUpdateTodo: (todoId: number) => void }) {
   const { onChangeChecked } = useCheckbox({ defaultValue: props.isDone })
@@ -18,19 +18,41 @@ export default function TodoListItem(props: Todo & { onUpdateTodo: (todoId: numb
   return (
     <li className={listClassName}>
       <label htmlFor={`todo-list-item-${props.id}`} className={classes["todo-list-item__label"]}>
-        <p className={classes["todo-list-item__contents"]}>{props.title}</p>
+        <motion.p
+          className={classes["todo-list-item__contents"]}
+          animate={props.isDone ? "checked" : "unChecked"}
+          initial={false}
+          variants={todoContentsAnimatmion}
+        >
+          {props.title}
+        </motion.p>
       </label>
       <div className={classes["todo-list-item__checkbox"]}>
-        {props.isDone && (
-          <label htmlFor={`todo-list-item-${props.id}`}>
-            <MdCheck color="#fff" />
-          </label>
-        )}
-        <Input
+        <motion.svg
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          initial={false}
+          animate={props.isDone ? "checked" : "unChecked"}
+        >
+          <motion.path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 12.75l6 6 9-13.5"
+            variants={checkboxSVGAnimation}
+            stroke="#fff"
+          />
+        </motion.svg>
+
+        <motion.input
           type="checkbox"
           id={`todo-list-item-${props.id}`}
           checked={props.isDone}
           onChange={handleChangeChecked}
+          initial={false}
+          animate={props.isDone ? "checked" : "unChecked"}
+          variants={checkboxAnimation}
         />
       </div>
     </li>
