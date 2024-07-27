ff43f34fece6a49a5cb08b7b591c6219d4c35d1d
d5ng <dong5ffice@gmail.com>
2024-07-27 19:51:00 +0900

    🔥 Remove: 불필요한 코드 삭제

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -1,19 +1,15 @@
 import { useTodo } from "features/todo/hooks"
-import { TodoForm, TodoHeader, TodoLayout, TodoList } from "features/todo/components"
+import { TodoHeader, TodoLayout, TodoList } from "features/todo/components"
+import TodoButton from "features/todo/components/todo-button/todo-button"
 
 export default function Todo() {
   const { todos, handleCreateTodo, handleUpdateTodo } = useTodo()
 
   return (
-    <>
-      <TodoLayout>
-        <TodoHeader todos={todos} />
-        <TodoList todos={todos} onUpdateTodo={handleUpdateTodo} />
-      </TodoLayout>
-
-      <TodoLayout>
-        <TodoForm onCreateTodo={handleCreateTodo} />
-      </TodoLayout>
-    </>
+    <TodoLayout>
+      <TodoHeader todos={todos} />
+      <TodoList todos={todos} onUpdateTodo={handleUpdateTodo} />
+      <TodoButton onCreateTodo={handleCreateTodo} />
+    </TodoLayout>
   )
 }
