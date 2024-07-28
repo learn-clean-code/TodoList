3a582c96ed631de49a86722b801b85c07baabeca
d5ng <dong5ffice@gmail.com>
2024-07-28 11:07:26 +0900

    ✨ Feat: ESC 누르면 토글 닫히는 기능 추가

--- src/shared/@common/hooks/use-toggle.ts
+++ src/shared/@common/hooks/use-toggle.ts
@@ -1,10 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function useToggle(initialState: boolean) {
   const [isToggle, setIsToggle] = useState(initialState || false)
   const onOpenToggle = () => setIsToggle(true)
   const onCloseToggle = () => setIsToggle(false)
   const onToggle = () => setIsToggle((prevToggle) => !prevToggle)
+
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      if (event.code === "Escape") return onCloseToggle()
+    }
+
+    window.addEventListener("keydown", keyDownHandler)
+    return () => window.removeEventListener("keydown", keyDownHandler)
+  }, [])
 
   return {
     isToggle,
